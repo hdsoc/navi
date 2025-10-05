0fc028f127236ac1a45abaf0fce8a72e963b7e37
hdsoc <l3u8zlzt@caye.org.uk>
2025-10-05 08:31:13 +0800

    Add navigation data

---
+++ src/data/data.ts
@@ -0,0 +1,20 @@
+export const sites = [
+  {
+    title: "常用",
+    items: [
+      { name: "GitHub", url: "https://github.com/" },
+      { name: "百度", url: "https://www.baidu.com/" },
+      { name: "知乎", url: "https://www.zhihu.com/" },
+      // 添加你的其他链接...
+    ],
+  },
+  {
+    title: "工具",
+    items: [
+      { name: "CSDN", url: "https://www.csdn.net/" },
+      { name: "掘金", url: "https://juejin.cn/" },
+      // 添加你的其他链接...
+    ],
+  },
+  // 可以继续添加分组
+];
